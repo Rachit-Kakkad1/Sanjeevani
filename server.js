facf794170f95e3535e08a6f564b55b17db6eece
Rachit Kakkad <kakkadrachit1@gmail.com>
2026-05-17 11:40:00 +0530

    add scaling note to server

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const connectDB = require('./src/config/db');
 const { initMatcher } = require('./src/services/matcher');
 const logger = require('./src/utils/logger');
 
+// TODO: investigate adding clustering for scaling
 const server = http.createServer(app);
 const PORT = process.env.PORT || 5000;
 
